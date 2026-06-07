44d080ed52cd9fae651594b92ccf7fb094009083
wanguimbutu <wanguimbutu@gmail.com>
2026-06-07 23:59:13 +0300

    remove net( weight field

--- crystal_custom/crystal_customizations/page/delivery_note_manage/delivery_note_manage.py
+++ crystal_custom/crystal_customizations/page/delivery_note_manage/delivery_note_manage.py
@@ -13,7 +13,7 @@ def get_order_items(order_names):
     return frappe.db.get_all(
         "Sales Order Item",
         fields=["parent", "item_code", "item_name", "qty", "delivered_qty",
-                "uom", "weight_per_unit", "net_weight", "rate", "amount"],
+                "uom", "weight_per_unit", "rate", "amount"],
         filters=[["parent", "in", order_names]],
         limit=0,
     )
